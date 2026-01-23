5ecc0f1046a14b9bf3e40e15e0e05dcd2533dbf7
MILSIM Deploy <deploy@milsim.com>
2026-01-23 11:44:28 -0500

    fix: use NGINX_ENVSUBST_FILTER instead of custom entrypoint to solve CRLF and config issues

--- frontend/admin/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-# Explicitly replace ONLY ${BACKEND_URL} and keep other variables (like $host, $uri) intact
-envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-
-exec "$@"
